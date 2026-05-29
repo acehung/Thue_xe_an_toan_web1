febb7af72e8d289481842295caef10bc59d0472f
Acehung <luonghung040805@gmail.com>
2026-05-29 11:37:20 +0700

    Update Gorent UI and clean tracked artifacts

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,21 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
+
 from api.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),  # Thêm đường dẫn cho trang web
+    path('', index, name='index'),
     path('api/', include('api.urls')),
 ]
+
+if getattr(settings, 'ENVIRONMENT', 'development') != 'production':
+    urlpatterns += [
+        re_path(
+            r'^static/(?P<path>.*)$',
+            serve,
+            {'document_root': settings.STATICFILES_DIRS[0]},
+        ),
+    ]
